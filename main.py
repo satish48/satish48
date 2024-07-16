8dcdc82505e4652057b0a71c060e669e385adec5
satish48 <satishmudrakola91@gmail.com>
2024-07-16 08:33:05 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Welcome to restaurant name generator.")
+person  =  input("who is your favourite person?\n")
+food = input("what is your favourite food?\n")
+print("your restaurant name could be " + person + " " +food)
+print("thank you!")
+#https://replit.com/@satishmudrakola/restaurant-name-generator-end
